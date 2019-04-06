5db4e3cf7b03c8fff9216be530f536e9b3e3006c
corrys <49328474+corrys@users.noreply.github.com>
2019-04-06 19:11:36 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,66 +8,19 @@ bot.on("ready", function() {
     console.log("The bot have succesfuly joined the server");
 });
 
-bot.on('message', message => {
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
 
-	if(message.author.bot) return
+    if(!message.content.startsWith(PREFIX)) return;
 
+    var args = message.content.substring(PREFIX.length).split(" ");
 
-	if(['Salut', 'salut'].includes(message.content)) {
-
-		message.reply('Salut ✋')
-	}
-
-	if(['ki joue' , 'qui joue ?', 'Qui joue', 'Qui joue ?', 'qui joue'].includes(message.content)) {
-		message.reply('Moi ! Je veux jouer avec toi !')
-	}
-
-
-	if(['a+' , "A+" , "Good bye"].includes(message.content)) {
-		message.reply('A+ ! 👋')
-	}
-
-
-	if(message.content.includes('cool le bot')) {
-		message.reply('Merci ! CoRiCo02 qui la coder 😉')
-	}
-
-
-	if(['wsh' , 'Wsh'].includes(message.content)) {
-		message.reply("Wsh la cité !")
-	}
-
-
-	if(message.content.startsWith(prefix)){
-		const args = message.content.slice(prefix.length).trim().split(/ +/g);
-      const command = args.shift().toLowerCase();
-
-
-		if command == ("clap"){
-			message.reply('applaudit !👏👏👏')
-			console.log("Commande !clap demander")
-		}
-
-		if command === 'stop') {
-			if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply(`Tu n'as pas la permission d'executer cette commande`)
-			message.reply('Arret du bot...')
-			console.log("Arret du bot demander")
-			bot.destroy()
-		}
-
-		if command == "invite"){
-			message.reply("**MAINTENANCE EN COURS**")
-			console.log("Invitation demander")
-		}
-
-
-
-		if command == "ping"){
-			message.channel.send("**MAINTENANCE EN COURS** : Le Ping l'API est de `"+ bot.ping+"` ms")
-		}
-	}
-})
-
-
+    switch(arg[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("test")
+        break;
+    }
+    
+});
 
 bot.login(process.env.TOKEN);
