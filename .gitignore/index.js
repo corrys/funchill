fa2fd30ee534fbf74c44610763153169d9f2b341
corrys <49328474+corrys@users.noreply.github.com>
2019-04-06 23:03:06 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,67 +8,23 @@ bot.on("ready", function() {
     console.log("The bot have succesfuly joined the server");
 });
 
-var rules1 = new Discord.RichEmbed()
-        .addField("Rules (1/2):", "---------------------------------------------------------------------------------------\n" +
-                                  "__**1.**__ No spamming or flooding the chat with messages, symbols or pictures etc. \n" +
-                                  "__**2.**__ Do not type in all Caps, Bold, Italic or other formats unless you're a  Admin. \n" +
-                                  "__**3.**__ adult (18+), explicit images etc, go to the NSFW channel \n" +
-                                  "__**4.**__ No racist or degrading content.\n" +
-                                  "__**5.**__ No excessively cursing. \n" +
-                                  "__**6.**__ No advertising other sites/discord servers without permission. \n" +
-                                  "__**7.**__ No posting external links other than direct links to youtube\n", true)
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
 
-        .setColor("0xFF0000")
+    if(!message.content.startsWith(PREFIX)) return;
 
-var rules2 = new Discord.RichEmbed()
-        .addField("Rules (2/2):", "__**8.**__ No using peoples usernames and/or posing as them. \n" +
-                                  "__**9.**__ No begging or repeatedly asking for permissions in the chat. \n" +
-                                  "__**10.**__. No offensive names or names which contain swear words. \n" +
-                                  "__**11.**__ Do not argue with staff there decisions are final. \n" +
-                                  "__**12.**__ Do not repeatedly message Moderators or Admins without permission \n" +
-                                  "__**13.**__ Do not message people, Moderators or Admins with \"Stupid\" questions \n" +
-                                  "---------------------------------------------------------------------------------------\n", true)
+    var args = message.content.substring(PREFIX.length).split(" ");
 
-        .setColor("0xFF0000")
+    switch(arg[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("test")
+        break;
+    }
 
-var role = new Discord.RichEmbed()
-        .addField("Role Hierarchy", "-------------------------------------------------------------------------------------------------\n" +
-                                    "__**Owner:**__ Highest Ranking Person On The Server always respect them or be penalized! \n" +
-                                    "__**Bots:**__ These Are All of the bots on this server \n" +
-                                    "__**Members:**__ Lowest Ranking person on the server, they have basic permissions and cannot access admin chanels \n" +
-                                    "-------------------------------------------------------------------------------------------------\n", true)
-
-        .setColor(EmbedColors[Math.floor(Math.random() * EmbedColors.length)])
-
-function getStatData(location, message , $){
-
-  var selector = $('.stats-stat .value').eq(location).text();
-
-  var stat_array = $.parseHTML(selector);
-
-  var stat = 0;
-
-  if(stat_array == null || stat_array.lengh == 0){
-    message.channel.send("Invalid User");
-    return " ";
-  }else{
-    stat = stat_array[0].data;
-  }
-
-  return stat;
-}
-
-function play(connection, message){
-    var server = servers[message.guild.id];
-
-    const streamOptions = { volume : 0.50}
-    const stream = YTDL(server.queue[0], {filter: "audioonly"});
-
-    server.dispatcher = connection.playStream(stream, streamOptions);
-
-    NOW_PLAYING = server.queue[0];
-
-    server.queue.shift();
-}
+    if(message.content === 'funchill') {
+        message.reply('<3')
+    }
+    
+});
 
 bot.login(process.env.TOKEN);
