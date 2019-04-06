036d21ce188b5cd25fb6072de041a37a461d78a4
corrys <49328474+corrys@users.noreply.github.com>
2019-04-06 18:47:51 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,9 +1,25 @@
 const Discord = require("discord.js");
 
+var PREFIX = "?";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     console.log("The bot have succesfuly joined the server");
+});
+
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(arg[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("test")
+        break;
+    }
 });
 
 bot.login(process.env.TOKEN);
