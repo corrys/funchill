6f8173f129a15fb11f5af3e16e7ba3e18effbd6d
corrys <49328474+corrys@users.noreply.github.com>
2019-04-06 23:42:15 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,19 +10,4 @@ bot.on("ready", function() {
     console.log("The bot have succesfuly joined the server");
 });
 
-bot.on("message", async function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if(!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(arg[0].toLowerCase()) {
-        case "invite":
-        message.channel.send("test")
-        break;
-    }
-    
-});
-
 bot.login(process.env.TOKEN);
