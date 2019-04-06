c3c321b633338bb5264606512801a94e32ad5725
corrys <49328474+corrys@users.noreply.github.com>
2019-04-06 02:51:33 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("I'm a just a test made by CoRiCo02");
     console.log("The bot have succesfuly joined the server");
 });
 
